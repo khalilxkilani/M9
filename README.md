49ba9572c83a543edd9999f27cbd2d807884d071
Khalil Kilani <102847594+khalilxkilani@users.noreply.github.com>
2024-12-16 15:14:47 -0500

    Added movement and layers.

--- README.md
+++ README.md
@@ -40,3 +40,5 @@ Text.
 * [Drawing Lines](https://p5js.org/examples/animation-and-variables-drawing-lines/) on p5.js.
 * [1.4 Draw With Mouse](https://nycdoe-cs4all.github.io/units/2/lessons/lesson_1.4) on NYC Department of Education.
 * [endShape()](https://p5js.org/reference/p5/endShape/) on p5.js.
+* [2.6: createGraphics() - p5.js Tutorial](https://youtu.be/TaluaAD9MKA?si=-PxropzQQv_ftfrA) on The Coding Train.
+* [Create Graphics](https://p5js.org/examples/advanced-canvas-rendering-create-graphics/) on p5.js.
--- docs/sketch.js
+++ docs/sketch.js
@@ -9,17 +9,26 @@
 let clouds = []; // Array of Clouds
 let current_line_coordinates = [];
 let is_paused = false;
+let is_drawing = false;
+let draw_stroke_layer;
 
-
-// Initialize canvas at the start of every new sketch.
+// Initialize canvas and drawing buffer at the start of every new sketch.
 function setup() {
     createCanvas(innerWidth, innerHeight);
+
+    // Initialize graphics buffer for intermediate draw strokes
+    draw_stroke_layer = createGraphics(innerWidth, innerHeight);
+    draw_stroke_layer.show();
 }
 
 
-// Resize the canvas when the window's size changes.
+// Resize the canvas and drawing buffer when the window's size changes.
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+
+    draw_stroke_layer.remove(); // Remove graphics buffer from web page
+    draw_stroke_layer = undefined; // Delete graphics buffer from CPU memory
+    draw_stroke_layer = createGraphics(innerWidth, innerHeight);
 }
 
 
@@ -35,23 +44,39 @@ function check_pause() {
 
 // Draw and store the line coordinates of every mouse drag.
 function mouseDragged() {
-    strokeWeight(2);
-    // Make line by connecting previous mouse coordinates
-    line(pmouseX, pmouseY, mouseX, mouseY);
-    current_line_coordinates.push([mouseX, mouseY]);
+    if (!is_paused) { // Only draw if user has allowed movement
+        is_drawing = true;
+        draw_stroke_layer.strokeWeight(2);
+
+        // Make line by connecting previous mouse coordinates
+        draw_stroke_layer.line(pmouseX, pmouseY, mouseX, mouseY);
+        current_line_coordinates.push([mouseX, mouseY]);
+    }
 }
 
 
 // Create a Cloud using stored line coordinates upon mouse drag completion.
 function mouseReleased() {
-    clouds.push(new Cloud(this, current_line_coordinates)); // Create a Cloud
-    current_line_coordinates = []; // Reset line coordinates for next Cloud
+    if (is_drawing) { // Only create Cloud if one was drawn (omit mouse presses)
+        is_drawing = false;
+        clouds.push(new Cloud(this, current_line_coordinates)); // Create Cloud
+        current_line_coordinates = []; // Reset line coordinates for next Cloud
+        draw_stroke_layer.clear(); // Clear the drawing buffer
+    }
 }
 
 
 function draw() {
+    clear(); // Clear the background
+    
+    image(draw_stroke_layer, 0, 0); // Display drawing buffer from top left
+
+    check_pause();
     // Display each cloud
     clouds.forEach((cloud) => {
+        if (!is_paused) { // Only move clouds if user has allowed movement
+            cloud.move();
+        }
         cloud.display();
     });
 }
@@ -61,6 +86,10 @@ class Cloud {
     constructor(canvas, coordinates) {
         this.canvas = canvas; // Store a reference to the canvas
         this.coordinates = coordinates; // Coordinates of the bounding line
+
+        // TODO: change speed
+        this.speedX = this.canvas.random(-5, 5);
+        this.speedY = this.canvas.random(-5, 5);
     }
 
     display() {
@@ -77,6 +106,11 @@ class Cloud {
     }
 
     move() {
-        // TODO
+        // Update the position of the cloud according to its speed
+        this.coordinates.forEach((coordinate) => {
+            coordinate[0] += this.speedX;
+            coordinate[1] += this.speedY;
+        });
+
     }
 }
