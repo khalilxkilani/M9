7f0c059eb808ecc534b213f83a58d469b76ef028
Khalil Kilani <102847594+khalilxkilani@users.noreply.github.com>
2024-12-18 01:09:27 -0500

    Made clouds bounce off the window walls.

--- README.md
+++ README.md
@@ -33,18 +33,24 @@ Text.
 Text.
 
 ### Sources:
+**Processing:**
 * [Processing Reference](https://processing.org/reference) on Processing.
-* [Include a Processing File (.pde) in a Webpage](https://home.et.utwente.nl/slootenvanf/2017/10/03/include-processing-file-in-webpage/) on VanSlooten.
-* [Hosting a p5.js Sketch with Github Pages](https://youtu.be/ZneWjyn18e8?feature=shared) on The Coding Train.
 * [Creating and Styling HTML](https://p5js.org/tutorials/creating-styling-html/) on p5.js.
 * [Drawing Lines](https://p5js.org/examples/animation-and-variables-drawing-lines/) on p5.js.
-* [1.4 Draw With Mouse](https://nycdoe-cs4all.github.io/units/2/lessons/lesson_1.4) on NYC Department of Education.
-* [endShape()](https://p5js.org/reference/p5/endShape/) on p5.js.
-* [2.6: createGraphics() - p5.js Tutorial](https://youtu.be/TaluaAD9MKA?si=-PxropzQQv_ftfrA) on The Coding Train.
 * [Create Graphics](https://p5js.org/examples/advanced-canvas-rendering-create-graphics/) on p5.js.
-* [Color Gradients by Jules Kris and Tristan Bunn](https://p5js.org/tutorials/color-gradients/) on p5.js.
+* [endShape()](https://p5js.org/reference/p5/endShape/) on p5.js.
+* [noise()](https://p5js.org/reference/p5/noise/) on p5.js.
+* [Color Gradients](https://p5js.org/tutorials/color-gradients/) by Jules Kris and Tristan Bunn on p5.js.
+
+**YouTube:**
+* [Hosting a p5.js Sketch with Github Pages](https://youtu.be/ZneWjyn18e8?feature=shared) by The Coding Train on YouTube.
+* [2.6: createGraphics() - p5.js Tutorial](https://youtu.be/TaluaAD9MKA?si=-PxropzQQv_ftfrA) by The Coding Train on YouTube.
+* [How to Make Objects Bounce Off Walls in the p5.js Programming Language](https://youtu.be/Kp070rI_G48?feature=shared) by Jason Erdreich on YouTube.
+
+**Miscellaneous:**
+* [p5.collide2D Library](https://github.com/bmoren/p5.collide2D.git) on GitHub.
+* [Include a Processing File (.pde) in a Webpage](https://home.et.utwente.nl/slootenvanf/2017/10/03/include-processing-file-in-webpage/) on VanSlooten.
+* [1.4 Draw With Mouse](https://nycdoe-cs4all.github.io/units/2/lessons/lesson_1.4) on NYC Department of Education.
 * [Color Picker](https://htmlcolorcodes.com) on HTML Color Codes.
 * [Sunny Icon ICO](https://www.shareicon.net/sunny-307412) on ShareIcon.
 * [How to Create and Implement an HTML Favicon](https://www.dhiwise.com/post/how-to-create-and-implement-an-html-favicon) on DhiWise.
-* [noise()](https://p5js.org/reference/p5/noise/) on p5.js.
-* [p5.collide2D Library](https://github.com/bmoren/p5.collide2D.git) on GitHub.
--- docs/sketch.js
+++ docs/sketch.js
@@ -7,98 +7,108 @@
 
 
 let clouds = []; // Array of Clouds
-let current_line_coordinates = [];
-let is_paused = false;
-let is_drawing = false;
-let sky_layer;
-let draw_stroke_layer;
-let prev_num_clouds = -1;
-let cloud_colors = [
+let currLinePoints = [];
+let cloudColors = [
     [230, 230, 250], [255, 245, 238], [255, 228, 225], [220, 220, 220]
 ]; // RGB values for Lavender, SeaShell, MistyRose, and Gainsboro
+let isPaused = false;
+let isDrawing = false;
+let skyLayer;
+let drawStrokeLayer;
+let prevNumClouds = -1; // Set negative so program can use range 0 <= i <= n
+const SKY_GRADIENT_RATE = 10; // Rate at which gradient changes colors
+const TRANSPARENCY_RATE = 1; // Rate at which clouds lose transparency
 
 
-const SKY_COLOR_CHANGE_RATE = 10;
-const TRANSPARENCY_CHANGE_RATE = 1;
-
-
-// Initialize canvas and drawing buffer at the start of every new sketch.
+/**
+ * Initialize canvas and drawing buffer at the start of every new sketch.
+ */
 function setup() {
     createCanvas(innerWidth, innerHeight);
 
     // Initialize graphics buffer for intermediate draw strokes
-    sky_layer = createGraphics(innerWidth, innerHeight);
-    sky_layer.show();
+    skyLayer = createGraphics(innerWidth, innerHeight);
+    skyLayer.show();
 
     // Initialize graphics buffer for intermediate draw strokes
-    draw_stroke_layer = createGraphics(innerWidth, innerHeight);
-    draw_stroke_layer.show();
+    drawStrokeLayer = createGraphics(innerWidth, innerHeight);
+    drawStrokeLayer.show();
 }
 
 
-// Resize the canvas and drawing buffer when the window's size changes.
+/**
+ * Resize the canvas and drawing buffer when the window's size changes.
+ */
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 
-    sky_layer.remove(); // Remove graphics buffer from web page
-    sky_layer = undefined; // Delete graphics buffer from CPU memory
-    sky_layer = createGraphics(innerWidth, innerHeight);
-    sky_gradient(true);
+    skyLayer.remove(); // Remove graphics buffer from web page
+    skyLayer = undefined; // Delete graphics buffer from CPU memory
+    skyLayer = createGraphics(innerWidth, innerHeight);
+    displaySkyGradient(true);
 
-    draw_stroke_layer.remove(); // Remove graphics buffer from web page
-    draw_stroke_layer = undefined; // Delete graphics buffer from CPU memory
-    draw_stroke_layer = createGraphics(innerWidth, innerHeight);
+    drawStrokeLayer.remove(); // Remove graphics buffer from web page
+    drawStrokeLayer = undefined; // Delete graphics buffer from CPU memory
+    drawStrokeLayer = createGraphics(innerWidth, innerHeight);
 }
 
 
-// Check if the user has paused movement to save their creation.
-function check_pause() {
+/**
+ * Check if the user has paused movement to save their creation.
+ */
+function checkPause() {
     if (isKeyPressed && keyCode === UP_ARROW) { // User pressed up arrow
-        is_paused = true;
+        isPaused = true;
     } else if (isKeyPressed) {
-        is_paused = false;
+        isPaused = false;
     }
 }
 
 
-// Draw and store the line coordinates of every mouse drag.
+/**
+ * Draw and store the line coordinates of every mouse drag.
+ */
 function mouseDragged() {
-    if (!is_paused) { // Only draw if user has allowed movement
-        is_drawing = true;
-        draw_stroke_layer.strokeWeight(2);
+    if (!isPaused) { // Only draw if user has allowed movement
+        isDrawing = true;
+        drawStrokeLayer.strokeWeight(2);
 
         // Make line by connecting previous mouse coordinates
-        draw_stroke_layer.line(pmouseX, pmouseY, mouseX, mouseY);
-        current_line_coordinates.push([mouseX, mouseY]);
+        drawStrokeLayer.line(pmouseX, pmouseY, mouseX, mouseY);
+        currLinePoints.push([mouseX, mouseY]);
     }
 }
 
 
-// Create a Cloud using stored line coordinates upon mouse drag completion.
+/**
+ * Create a Cloud using stored line coordinates upon mouse drag completion.
+ */
 function mouseReleased() {
-    if (is_drawing) { // Only create Cloud if one was drawn (omit mouse presses)
-        is_drawing = false;
-        clouds.push(new Cloud(this, current_line_coordinates)); // Create Cloud
-        current_line_coordinates = []; // Reset line coordinates for next Cloud
-        draw_stroke_layer.clear(); // Clear the drawing buffer
+    if (isDrawing) { // Only create Cloud if one was drawn (omit mouse presses)
+        isDrawing = false;
+        clouds.push(new Cloud(currLinePoints)); // Create Cloud
+        currLinePoints = []; // Reset line coordinates for next Cloud
+        drawStrokeLayer.clear(); // Clear the drawing buffer
     }
 }
 
 
+/**
+ * Draw the sky gradient and Clouds every frame.
+ */
 function draw() {
     clear(); // Clear the background
-
-    sky_gradient();
+    displaySkyGradient();
 
     // Display drawing buffers from top left
-    image(sky_layer, 0, 0);
-    image(draw_stroke_layer, 0, 0);
+    image(skyLayer, 0, 0);
+    image(drawStrokeLayer, 0, 0);
 
-    check_pause();
+    checkPause();
     // Display each cloud
-    clouds = clouds.filter(check_cloud_bounds);
+    clouds = clouds.filter(checkCloudBounds).filter(checkTransparency);
     clouds.forEach((cloud) => {
-        if (!is_paused) { // Only move clouds if user has allowed movement
+        if (!isPaused) { // Only move clouds if user has allowed movement
             cloud.move();
             cloud.dissipate();
         }
@@ -107,44 +117,55 @@ function draw() {
 }
 
 
-// Define the blue and grey hues of the sky gradient depending on the number of
-// clouds present. Default parameter assumes window has not been resized.
-function sky_gradient(is_window_resized = false) {
-    let num_clouds = clouds.length; // Obtain number of clouds currently visible
+/**
+ * Define the blue and grey hues of the sky gradient depending on the number of
+ * clouds present.
+ * 
+ * @param {*} is_window_resized true if window has been resized, false otherwise
+ * @returns does not execute if number of Clouds has not changed
+ */
+function displaySkyGradient(is_window_resized = false) {
+    let numClouds = clouds.length; // Obtain number of clouds currently visible
 
     // Only redraw the sky gradient if the number of clouds has changed
-    if (!is_window_resized && num_clouds === prev_num_clouds) {
+    if (!is_window_resized && numClouds === prevNumClouds) {
         return;
     } else {
-        prev_num_clouds = num_clouds;
-        sky_layer.clear(); // Clear the sky buffer
+        prevNumClouds = numClouds;
+        skyLayer.clear(); // Clear the sky buffer
     }
 
     // Adjust B value down for darker, up for lighter
-    let blue_hue = color(0, 150 - (num_clouds * SKY_COLOR_CHANGE_RATE),
-        255 - (num_clouds * SKY_COLOR_CHANGE_RATE));
+    let blueHue = color(0, 150 - (numClouds * SKY_GRADIENT_RATE),
+        255 - (numClouds * SKY_GRADIENT_RATE));
 
     // Adjust R,G,B values down for darker, up for lighter
-    let grey_RGB_value = 225 - (num_clouds * SKY_COLOR_CHANGE_RATE);
-    let grey_hue = color(grey_RGB_value, grey_RGB_value, grey_RGB_value);
+    let greyRGB = 225 - (numClouds * SKY_GRADIENT_RATE);
+    let greyHue = color(greyRGB, greyRGB, greyRGB);
 
     // Draw horizontal lines per height pixel using a different blue-grey blend
     for (let y = 0; y < innerHeight; y += 1) {
         // Determine how much to interpolate between both colors
         let amount = map(y, 0, innerHeight, 0, 1);
         // Lerp to blend both colors
-        let gradient = lerpColor(blue_hue, grey_hue, amount);
-        sky_layer.stroke(gradient);
-        sky_layer.line(0, y, innerWidth, y);
+        let gradient = lerpColor(blueHue, greyHue, amount);
+        skyLayer.stroke(gradient);
+        skyLayer.line(0, y, innerWidth, y);
     }
 }
 
 
-// Determine if a Cloud is out of the window bounds.
-function check_cloud_bounds(cloud) {
-    [xCoord, yCoord] = cloud.get_first_coord_pair();
+/**
+ * Determine if a Cloud is outside of the window bounds. Not a Cloud method as
+ * this function is used when performing Array filtering.
+ * 
+ * @param {Cloud} cloud 
+ * @returns true if Cloud within bounds, false otherwise
+ */
+function checkCloudBounds(cloud) {
+    [xCoord, yCoord] = cloud.getFirstCoordPair();
 
-    // Check if within window bounds using quarter padding
+    // Quarter padding around window bounds
     if (xCoord > (innerWidth * 1.25) || xCoord < -(innerWidth / 4) ||
         yCoord > (innerHeight * 1.25) || yCoord < -(innerHeight / 4)) {
         return false;
@@ -154,25 +175,58 @@ function check_cloud_bounds(cloud) {
 }
 
 
+/**
+ * Determine if a Cloud has become fully transparent. Not a Cloud method as
+ * this function is used when performing Array filtering.
+ * 
+ * @param {Cloud} cloud 
+ * @returns true if Cloud is fully transparent, false otherwise
+ */
+function checkTransparency(cloud) {
+    if (cloud.transparency <= 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+/**
+ * Cloud objects are what the user draws and interacts with on the screen.
+ */
 class Cloud {
-    constructor(canvas, coordinates) {
-        this.canvas = canvas; // Store a reference to the canvas
-        this.coordinates = coordinates; // Coordinates of the bounding line
+
+    /**
+     * Initialize a new Cloud object.
+     * 
+     * @param {*} coordinates coordinates of the bounding line
+     */
+    constructor(coordinates) {
+        this.coordinates = coordinates;
 
         this.vectors = [];
-        this.color = random(cloud_colors);
+        this.color = random(cloudColors);
         this.transparency = 255;
 
         // TODO: change speed
-        this.speedX = this.canvas.random(-5, 5);
-        this.speedY = this.canvas.random(-5, 5);
+        this.speedX = random(-5, 5);
+        this.speedY = random(-5, 5);
+        this.directionX = Math.sign(this.speedX);
+        this.directionY = Math.sign(this.speedY);
+
+        this.isDirXChanged = false;
+        this.isDirYChanged = false;
+        this.isTouchingWall = false;
     }
 
+    /**
+     * Render the Cloud.
+     */
     display() {
         // Specify the fill color for the Cloud
         let [r, g, b] = this.color; // Unpack assigned color
         fill(r, g, b, this.transparency);
-        noStroke();
+        noStroke(); // Remove outline of Cloud shape
 
         this.vectors = []; // Reset vectors
 
@@ -180,39 +234,83 @@ class Cloud {
         beginShape();
         this.coordinates.forEach((coordinate) => {
             // Every 60 frames, offset the coordinates to perform wiggle effect
-            if (frameCount % 60 === 0 && !is_paused) {
+            if (frameCount % 60 === 0 && !isPaused) {
                 coordinate[0] += random(-10, 10) * noise(0.05 * frameCount);
                 coordinate[1] += random(-5, 5) * noise(0.05 * frameCount);
             }
             curveVertex(coordinate[0], coordinate[1]); // Connect using a vertex
 
             // Update vectors
-            let curr_vector = createVector(coordinate[0], coordinate[1]);
-            this.vectors.push(curr_vector);
+            let currVector = createVector(coordinate[0], coordinate[1]);
+            this.vectors.push(currVector);
         });
         endShape(CLOSE); // Connect first and last vertices
     }
 
+    /**
+     * Move the Cloud.
+     */
     move() {
-        // Update the position of the cloud according to its speed
+        // Check if any coordinate has surpassed the window boundaries
         this.coordinates.forEach((coordinate) => {
-            coordinate[0] += this.speedX;
-            coordinate[1] += this.speedY;
+            this.checkWallBounceX(coordinate[0]);
+            this.checkWallBounceY(coordinate[1]);
         });
 
+        // Update Cloud positon according to its speed and direction
+        this.coordinates.forEach((coordinate) => {
+            coordinate[0] += this.speedX * this.directionX;
+            coordinate[1] += this.speedY * this.directionY;
+        });
+
+        // Enable direction changes on next call to move()
+        this.isDirXChanged = false;
+        this.isDirYChanged = false;
     }
 
-    // Return the first coordinate pair (x, y) of the cloud.
-    get_first_coord_pair() {
+    /**
+     * Return the first (x, y) coordinate pair of the cloud.
+     * 
+     * @returns first (x, y) coordinate pair of the cloud.
+     */
+    getFirstCoordPair() {
         return this.coordinates[0];
     }
 
-    // If mouse hovers over cloud, make it turn transparent
+    /**
+     * If the mouse hovers over the Cloud, increase transparency of the Cloud.
+     */
     dissipate() {
         // Check if mouse collides with cloud
         let hit = collidePointPoly(mouseX, mouseY, this.vectors);
         if (hit) {
-            this.transparency -= TRANSPARENCY_CHANGE_RATE; // Decrease alpha value
+            this.transparency -= TRANSPARENCY_RATE; // Decrease alpha value
+        }
+    }
+
+    /**
+     * Check if the Cloud should bounce off the left or right bounds of the
+     * window.
+     * 
+     * @param {*} xCoord a single x coordinate within the Cloud's shape.
+     */
+    checkWallBounceX(xCoord) {
+        if ((xCoord < 0 || xCoord > innerWidth) && !this.isDirXChanged) {
+            this.directionX *= -1;
+            this.isDirXChanged = true;
+        }
+    }
+
+    /**
+     * Check if the Cloud should bounce off the top or bottom bounds of the
+     * window.
+     * 
+     * @param {*} yCoord a single y coordinate within the Cloud's shape.
+     */
+    checkWallBounceY(yCoord) {
+        if ((yCoord < 0 || yCoord > innerHeight) && !this.isDirYChanged) {
+            this.directionY *= -1;
+            this.isDirYChanged = true;
         }
     }
 }
